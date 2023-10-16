c1d377ec5844d65013c594929900f994a97126b6
FabienTavernier <fabien.tavernier@oclock.io>
2023-10-16 10:09:55 +0200

    About : commentaire

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+/*
+  équivalent au composant de React Router
+
+  <Route path="/about" element={<About />} />
+*/
+
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
